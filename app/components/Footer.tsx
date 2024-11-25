e4b9038871f6ad65f23cb6e37312762909b81ee0
annaa <annaalehed@hotmail.com>
2024-11-25 14:43:16 +0100

    start of project, install firebase and footer,header

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-pink-300">
+      <h2>C sedan 2024</h2>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="bg-slate-600">
+      <h1>Stage Stories</h1>
+    </header>
+  );
+}
+
+export default Header;
