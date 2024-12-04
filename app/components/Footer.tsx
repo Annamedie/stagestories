2b333be178504b5ce5330597053fd372927bea55
annaa <annaalehed@hotmail.com>
2024-12-04 11:00:06 +0100

    header and footer not visiblie with auth

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,4 +1,14 @@
+"use client";
+import { usePathname } from "next/navigation";
+
 function Footer() {
+  const pathname = usePathname() || "";
+
+  const isAuthPage = pathname.startsWith("/auth");
+
+  if (isAuthPage) {
+    return null;
+  }
   return (
     <footer className="bg-pink-300">
       <h2>C sedan 2024</h2>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import LogOutButton from "./LogOutButton";
 import ProfileButton from "./ProfileButton";
 
 function Header() {
+  const pathname = usePathname() || "";
+
+  const isAuthPage = pathname.startsWith("/auth");
+
+  if (isAuthPage) {
+    return null;
+  }
   return (
     <header className="bg-slate-600">
       <Link href="/">
