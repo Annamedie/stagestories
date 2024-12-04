664967ab11aa25b478e8612d4b5fb4725bc7d2cb
annaa <annaalehed@hotmail.com>
2024-12-04 17:23:19 +0100

    styling and star of remove of like button

--- app/components/ConcertCard.tsx
+++ app/components/ConcertCard.tsx
@@ -2,14 +2,14 @@
 import Image from "next/image";
 import { useState } from "react";
 import { likePost, unlikePost } from "../api/postActions";
+import TopTracks from "../svg/TopTracks.svg";
 import { Post } from "../types/dataTypes";
 interface ConcertCardProps {
   post: Post;
-  params?: { slug?: string; id?: string };
+  isProfile?: boolean;
 }
 
-function ConcertCard({ post, params }: ConcertCardProps) {
-  const { slug } = params || {};
+function ConcertCard({ post, isProfile }: ConcertCardProps) {
   const [liked, setLiked] = useState(post.likesBy?.includes("userId"));
   const [likes, setLikes] = useState(post.likes || 0);
 
@@ -30,7 +30,7 @@ function ConcertCard({ post, params }: ConcertCardProps) {
 
   return (
     <>
-      <h2>{post.username}</h2>
+      <h2>{post.username ? post.username : "Hemligt"}</h2>
       <div key={post.id} className="border rounded-lg p-4 shadow-md">
         <div className="flex justify-between items-center mb-2">
           <Image
@@ -42,6 +42,9 @@ function ConcertCard({ post, params }: ConcertCardProps) {
           />
           <h2 className="text-xl font-semibold">{post.artistBand}</h2>
           <span className="text-sm text-gray-500">{post.rating}/5</span>
+          <span className="text-sm text-gray-500">
+            {isProfile && post.genre}
+          </span>
         </div>
         <p className="text-gray-700 mb-2">
           {post.review
@@ -55,6 +58,13 @@ function ConcertCard({ post, params }: ConcertCardProps) {
           <span className="text-sm text-gray-500">
             {new Date(post.showDate).toLocaleDateString()}
           </span>
+        </div>
+        <div>
+          {isProfile && post.topTracks && (
+            <div>
+              <TopTracks width={20} />
+            </div>
+          )}
         </div>
         <button
           onClick={handleLike}
