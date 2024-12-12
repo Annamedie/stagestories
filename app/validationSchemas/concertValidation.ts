6f5ee2669ad05112c4c8f4223842a9fa084629fc
annaa <annaalehed@hotmail.com>
2024-12-12 20:16:47 +0100

    more validation rules

--- app/validationSchemas/concertValidation.ts
+++ app/validationSchemas/concertValidation.ts
@@ -3,10 +3,10 @@ import { z } from "zod";
 export const ConcertFormSchema = z.object({
   artistBand: z
     .string()
-    .min(1, { message: "Artist or Band name must be at least 2 characters" }),
+    .min(1, { message: "Artist or Band name must be at least 1 character" }),
   location: z
     .string()
-    .min(1, { message: "Location must be at least 2 characters" }),
+    .min(1, { message: "Location must be at least 1 character" }),
   showDate: z
     .string()
     .refine((val) => !isNaN(Date.parse(val)), { message: "Invalid date" }),
@@ -15,8 +15,14 @@ export const ConcertFormSchema = z.object({
     .int()
     .min(1, { message: "Rating is required" })
     .max(5, { message: "Rating must be between 1 and 5" }),
-  venue: z.string().optional(),
-  tourName: z.string().optional(),
+  venue: z
+    .string()
+    .max(150, { message: "Venue name cannot exceed 150 characters " })
+    .optional(),
+  tourName: z
+    .string()
+    .max(150, { message: "Tour name cannot exceed 150 characters " })
+    .optional(),
   topTracks: z.array(z.string().optional()).max(3),
   genre: z.string().optional(),
   review: z
