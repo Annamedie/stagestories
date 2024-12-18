8248faf9d3d65ab751f1887020aa570e1a789028
annaa <annaalehed@hotmail.com>
2024-12-18 15:56:54 +0100

    enhance ConcertCard layout and styling for better responsiveness and visual appeal

--- app/components/ConcertCard.tsx
+++ app/components/ConcertCard.tsx
@@ -21,13 +21,13 @@ function ConcertCard({ post, isProfile }: ConcertCardProps) {
       {/* Card Content */}
       <div
         key={post.id}
-        className="lg:h-52 w-2/3 bg-[#F3F0E8] flex flex-col lg:flex-row lg:w-full overflow-hidden relative md:hover:scale-105 md:transition-transform md:duration-300 md:ease-in-out"
+        className="  xl:h-52 lg:h-60 w-2/3  bg-[#F3F0E8] flex flex-col lg:flex-row lg:w-full  relative lg:hover:scale-105 md:transition-transform md:duration-300 md:ease-in-out"
       >
-        <div className="absolute lg:right-[103px] lg:-translate-x-1/2 lg:-top-4 bg-[#020C11] w-6 h-6 rounded-full  "></div>
+        <div className=" hidden md:block absolute lg:right-[103px] lg:-translate-x-1/2 lg:-top-4 bg-[#020C11] w-6 h-6 rounded-full max-lg:bottom-[84px] max-lg:-translate-y-5 max-lg:-translate-x-3 over"></div>
         <div
-          className="absolute 
-                lg:right-[103px] lg:-translate-x-1/2 lg:-bottom-4
-                max-lg:bottom-16 max-lg:left-[510px] max-lg:-translate-x-8 max-lg:-translate-y-3
+          className=" hidden md:block absolute 
+                lg:right-[103px] lg:-translate-x-1/2 lg:-bottom-8 
+                bottom-[84px] -translate-y-5 translate-x-[480px]
                 bg-[#020C11] w-6 h-6 rounded-full"
         ></div>
 
@@ -44,15 +44,17 @@ function ConcertCard({ post, isProfile }: ConcertCardProps) {
             </div>
             <div className="lg:pl-4 lg:pt-4 flex flex-col lg:justify-between items-center lg:items-stretch text-center lg:text-left">
               <div>
-                <h2 className="text-2xl font-semibold">{post.artistBand}</h2>
-                <h3 className="font-medium italic">
-                  {post.tourName ? post.tourName : "Sunny Tour"}
+                <h2 className="text-2xl lg:text-lg xl:text-2xl font-semibold">
+                  {post.artistBand}
+                </h2>
+                <h3 className="font-medium lg:text-sm xl:text-lg text-lg italic">
+                  {post.tourName}
                 </h3>
               </div>
-              <div className="my-1">
-                <p>{post.venue ? post.venue : "Ullevi"}</p>
+              <div className="my-1 text-base lg:text-xs xl:text-base">
+                <p>{post.venue}</p>
                 <p>{post.location}</p>
-                <p className="text-sm text-gray-500 capitalize">{post.genre}</p>
+                <p className=" text-gray-700 capitalize">{post.genre}</p>
               </div>
               {post.rating && (
                 <div className="pb-2">
@@ -65,14 +67,14 @@ function ConcertCard({ post, isProfile }: ConcertCardProps) {
               )}
             </div>
           </div>
-          <div className="pt-2 pr-2 flex flex-col lg:items-end items-center justify-between flex-grow">
+          <div className="pt-2 xl:pr-2 pr-1 flex flex-col lg:items-end items-center justify-between flex-grow">
             <div className="pt-2">
               <span className="text-sm text-black lg:block hidden">
                 {new Date(post.showDate).toLocaleDateString()}
               </span>
             </div>
             <div>
-              <p className="text-gray-700 mb-2 font-bold">
+              <p className="text-gray-700 mb-2 font-bold lg:text-sm">
                 {post.review && "Read review..."}
               </p>
             </div>
@@ -85,21 +87,21 @@ function ConcertCard({ post, isProfile }: ConcertCardProps) {
             </div>
           </div>
         </div>
-        <div className="lg:border-l-2 border-t-2 border-black lg:w-32 flex-shrink-0 flex flex-col">
+        <div className="lg:border-l-[3px] lg:border-t-0 border-t-4 border-dashed md:border-solid border-[#020C11] lg:w-32 flex-shrink-0 flex flex-col">
           <div className="lg:block hidden">
             <p className="text-center text-sm p-1 truncate">
-              {post.username ? post.username : "Hemligt"}
+              {post.username ? post.username : "Anonymous"}
             </p>
           </div>
           <div className="flex flex-col lg:flex-row items-center justify-center">
             <div className="lg:block hidden">
               <Barcode />
             </div>
-            <div className="lg:hidden block">
+            <div className="lg:hidden block mt-2">
               <BarcodeSmall />
             </div>
-            <div className="lg:pt-2 p-2">
-              <span className="text-sm text-black lg:hidden block">
+            <div className="lg:pt-2 p-2 lg:hidden block">
+              <span className="text-sm text-black font-semibold ">
                 {new Date(post.showDate).toLocaleDateString()}
               </span>
             </div>
@@ -108,15 +110,15 @@ function ConcertCard({ post, isProfile }: ConcertCardProps) {
             </span>
           </div>
           <div className="lg:hidden block">
-            <p className="text-center text-sm p-1 truncate">
-              {post.username ? post.username : "Hemligt"}
+            <p className="text-center text-sm pb-3 font-semibold truncate">
+              {post.username ? post.username : "Anonymous"}
             </p>
           </div>
         </div>
       </div>
 
       {/* Like Button and Likes Count */}
-      <div className="mt-2">
+      <div className="mb-5 mt-2 mr-64 lg:mr-0">
         <LikeButton
           postId={post.id || ""}
           initialLikes={post.likes || 0}
