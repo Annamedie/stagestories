76109aa830f6caf735b1c67cb36258388f287581
annaa <annaalehed@hotmail.com>
2024-12-22 21:18:01 +0100

    feat: add search functionality to concert reviews; implement SearchBar component and filter posts based on user input

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { fetchPosts } from "./api/postActions";
 import ConcertCard from "./components/ConcertCard";
+import SearchBar from "./components/SearchBar";
 import { useAuth } from "./context/Authcontext";
 import { Post } from "./types/dataTypes";
 
 function Home() {
   const { user, isloading } = useAuth();
   const [posts, setPosts] = useState<Post[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
   const router = useRouter();
 
   useEffect(() => {
@@ -34,6 +36,23 @@ function Home() {
   if (!user) {
     return null;
   }
+  const searchFields = [
+    "username",
+    "artistBand",
+    "location",
+    "venue",
+    "genre",
+    "tourName",
+  ];
+
+  const filteredPosts = posts.filter((post) => {
+    const query = searchQuery.toLowerCase();
+
+    return searchFields.some((field) => {
+      const value = (post as Record<string, any>)[field];
+      return typeof value === "string" && value.toLowerCase().includes(query);
+    });
+  });
 
   return (
     <main
@@ -43,20 +62,25 @@ function Home() {
       <h1 className="text-2xl font-bold m-4 text-white font-inter">
         Concert Reviews
       </h1>
+      <SearchBar value={searchQuery} onChange={setSearchQuery} />
       <section
         className="grid lg:grid-cols-2 grid-cols-1 lg:gap-8"
         aria-label="Concert reviews listings"
       >
-        {posts.map((post) => (
-          <Link
-            key={post.id}
-            href={`/post/${post.id}`}
-            aria-label={`Read more about this concert: ${post.artistBand}`}
-            className="focus:outline focus:outline-2 focus:outline-buttonDarkHover rounded"
-          >
-            <ConcertCard key={post.id} post={post} />
-          </Link>
-        ))}
+        {filteredPosts.length > 0 ? (
+          filteredPosts.map((post) => (
+            <Link
+              key={post.id}
+              href={`/post/${post.id}`}
+              aria-label={`Read more about this concert: ${post.artistBand}`}
+              className="focus:outline focus:outline-2 focus:outline-buttonDarkHover rounded"
+            >
+              <ConcertCard key={post.id} post={post} />
+            </Link>
+          ))
+        ) : (
+          <p className="text-white">No results found for "{searchQuery}".</p>
+        )}
       </section>
     </main>
   );
